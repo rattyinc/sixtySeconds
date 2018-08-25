599e38a2ca5e7bd84293bb8f97b2a02186279f43
Nathan Radsavanh <nathan@nomoss.co>
2018-08-25 15:38:53 +1000

    ~ character creation updates

--- Assets/HeadModifier.cs
+++ Assets/HeadModifier.cs
@@ -6,6 +6,13 @@ public class HeadModifier : MonoBehaviour {
 
 	public Sprite[] heads;
 	private int headInt = 0;
+
+	public Sprite[] hairs;
+	private int hairInt;
+
+	public Color[] eyeColors;
+	private int eyeColorInt = 0;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -32,8 +39,63 @@ public class HeadModifier : MonoBehaviour {
                 headInt += 1;
             }
 		}
-		Debug.Log("Sprite name: "+gameObject.name);
 		SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
 		sr.sprite = heads[headInt];
 	}
+
+	public void updateHair(bool left)
+    {
+        if (left)
+        {
+            if (hairInt == 0)
+            {
+				hairInt = hairs.Length - 1;
+            }
+            else
+            {
+				hairInt -= 1;
+            }
+        }
+        else
+        {
+			if (hairInt == hairs.Length - 1)
+            {
+				hairInt = 0;
+            }
+            else
+            {
+				hairInt += 1;
+            }
+        }
+        SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+		sr.sprite = hairs[hairInt];
+    }
+
+	public void updateEyes(bool left)
+    {
+		if (left)
+        {
+            if (eyeColorInt == 0)
+            {
+				eyeColorInt = eyeColors.Length - 1;
+            }
+            else
+            {
+				eyeColorInt -= 1;
+            }
+        }
+        else
+        {
+			if (eyeColorInt == eyeColors.Length - 1)
+            {
+				eyeColorInt = 0;
+            }
+            else
+            {
+				eyeColorInt += 1;
+            }
+        }
+        SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+		sr.color = eyeColors[eyeColorInt];
+    }
 }
