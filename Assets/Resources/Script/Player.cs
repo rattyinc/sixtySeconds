2a088f9322edd37c8eb70c38b2e4931df7f5d80c
Nathan Radsavanh <nathan@nomoss.co>
2018-08-25 12:31:09 +1000

    ~ working on character select

---
+++ Assets/Resources/Script/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A)) {
+			Vector3 tempPos = gameObject.transform.position;
+			tempPos.x -= speed * Time.deltaTime;
+			gameObject.transform.position = tempPos;
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			Vector3 tempPos = gameObject.transform.position;
+            tempPos.x += speed * Time.deltaTime;
+            gameObject.transform.position = tempPos;
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			Vector3 tempPos = gameObject.transform.position;
+            tempPos.y += speed * Time.deltaTime;
+            gameObject.transform.position = tempPos;
+  		}
+		if (Input.GetKey(KeyCode.S)) {
+			Vector3 tempPos = gameObject.transform.position;
+            tempPos.y -= speed * Time.deltaTime;
+            gameObject.transform.position = tempPos;
+		}
+	}
+}
