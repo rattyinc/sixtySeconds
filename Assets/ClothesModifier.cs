cf19298a06ec714a46cf8cec65e8c6cf99f20799
Nathan Radsavanh <nathan@nomoss.co>
2018-08-25 15:08:39 +1000

    ~ character creation 70 percent

---
+++ Assets/ClothesModifier.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothesModifier : MonoBehaviour {
+
+	public Color[] colors;
+	private int colorInt = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void updateClothesColor(bool left) {
+		if (left)
+        {
+			if (colorInt == 0)
+            {
+				colorInt = colors.Length - 1;
+            }
+            else
+            {
+				colorInt -= 1;
+            }
+        }
+        else
+        {
+			if (colorInt == colors.Length - 1)
+            {
+				colorInt = 0;
+            }
+            else
+            {
+				colorInt += 1;
+            }
+        }
+        SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+		sr.color = colors[colorInt];
+	}
+}
---
+++ Assets/HeadModifier.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadModifier : MonoBehaviour {
+
+	public Sprite[] heads;
+	private int headInt = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void updateHead(bool left) {
+		if (left) {
+			if (headInt == 0) {
+				headInt = heads.Length - 1;
+			}
+			else {
+				headInt -= 1;
+			}
+		} else {
+			if (headInt == heads.Length - 1) {
+               headInt = 0;
+            }
+            else {               
+                headInt += 1;
+            }
+		}
+		Debug.Log("Sprite name: "+gameObject.name);
+		SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+		sr.sprite = heads[headInt];
+	}
+}
