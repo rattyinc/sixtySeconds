01c9409dc81cb0a1b9291dbaea0016e2064030d9
Torrel <rogerz1123@gmail.com>
2018-08-25 17:08:33 +1000

    More cutscene work and some scripts

---
+++ Assets/Resources/Script/NextScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextScene : MonoBehaviour {
+public GameObject Nextthing;
+public GameObject Oldthing;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		Nextthing.SetActive(true);
+		Debug.Log("CONTACT");
+		Oldthing.SetActive(false);
+	}
+
+}
